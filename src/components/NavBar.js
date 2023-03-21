138e883377907bdb88677989032630635aa20ebd
abenaTenkorang <abenagy16@gmail.com>
2023-03-21 20:42:27 +0000

    Add navntar componet

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NavBar extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <div className="navbar">
+          <h1 className="heading">Math Magicians</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default NavBar;
